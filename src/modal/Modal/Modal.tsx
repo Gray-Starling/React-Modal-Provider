c1981439b4ea6923b8c31a07b8734de456d7d74b
Sergey <skvorcovsa90@gmail.com>
2024-03-06 00:23:31 +1000

    moved the distribution logic into a separate function

--- src/modal/Modal/Modal.tsx
+++ src/modal/Modal/Modal.tsx
@@ -5,13 +5,17 @@ import style from './Modal.style.module.scss'
 const Modal: React.FC = () => {
 	const { modalComponent, closeModal } = useModal()
 
+	const handleClick = (e: React.MouseEvent<HTMLDivElement>) => {
+		e.stopPropagation()
+	}
+
 	if (!modalComponent) {
 		return null
 	}
 
 	return (
 		<div className={style.modal} onClick={closeModal}>
-			<div onClick={e => e.stopPropagation()}>{modalComponent}</div>
+			<div onClick={e => handleClick(e)}>{modalComponent}</div>
 		</div>
 	)
 }
